cfba3e0deb758eb19d9a35c5ee83dcac61c4dc59
TheTiscks <skretov006@gmail.com>
2025-03-31 00:58:39 +0300

    Added favicon and its support

--- built/app.py
+++ built/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import subprocess
 
 app = Flask(__name__)
@@ -60,6 +60,14 @@ def process():
         'words': words,
         'encrypted': encrypted
     })
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        'static',
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
