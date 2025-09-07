e5aef181f2d767108dd0af5ce3417f09354ba520
TheTiscks <skretov006@gmail.com>
2025-09-07 23:39:29 +0300

    Endeavour for better structure

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from .config import Config
+from .models import db, init_db
+
+def create_app():
+    app = Flask(__name__, static_folder=None)
+    app.config.from_object(Config)
+    db.init_app(app)
+    with app.app_context():
+        init_db()
+    from .routes import bp
+    app.register_blueprint(bp)
+    return app
