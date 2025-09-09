b1dfb34a44929ba849d6ccfd4290dfcaa1c7a81a
TheTiscks <skretov006@gmail.com>
2025-09-09 23:10:55 +0300

    Workflow fix

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from .config import Config
 from .models import db, init_db
 
 def create_app():
-    app = Flask(__name__, static_folder=None)
+    app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
     with app.app_context():
